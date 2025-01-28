91716e6805f0c570b53dc19ebd27231531afa541
Inamarie84 <123692666+Inamarie84@users.noreply.github.com>
2025-01-28 14:32:44 +0100

    installed tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}", "!./node_modules/**/*"],
+  content: [
+    "./index.html", // Include your main HTML file
+    "./**/*.{html,js}", // Watch all HTML and JS files inside the project
+    "!./node_modules/**/*", // Exclude node_modules directory
+  ],
   theme: {
     extend: {},
   },
