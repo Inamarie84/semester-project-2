0a76ffde8cb14b6408f53ef470d78c57ba4361a0
Inamarie84 <123692666+Inamarie84@users.noreply.github.com>
2025-02-05 16:02:53 +0100

    new api key

--- assets/js/api/constants.js
+++ assets/js/api/constants.js
@@ -1,6 +1,6 @@
 export const BASE_API_URL = "https://v2.api.noroff.dev";
 
-export const API_KEY = "6b8fb5cf-4a72-4400-8acc-57c145d25bca";
+export const API_KEY = "e321e480-4a35-4474-9d97-e54e746031e3";
 
 export const AUTH_REGISTER_URL = `${BASE_API_URL}/auth/register`;
 export const AUTH_LOGIN_URL = `${BASE_API_URL}/auth/login`;
