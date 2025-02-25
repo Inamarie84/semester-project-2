621cef6f31baeb5d214148dba5436153a6883567
Inamarie84 <123692666+Inamarie84@users.noreply.github.com>
2025-02-25 20:44:11 +0100

    added debugging router

--- assets/js/core/router.js
+++ assets/js/core/router.js
@@ -40,46 +40,45 @@ import { showMessage } from "../utils/dom/messageHandler.js"; // Import showMess
 import { handleSkeletonLoader } from "../utils/dom/skeletonLoader.js"; // Import handleSkeletonLoader
 
 export default async function router(pathname = window.location.pathname) {
-  try {
-    console.log("Current pathname:", pathname); // Debugging log
+  console.log("Current pathname:", pathname); // Debugging
 
+  try {
     switch (pathname) {
       case "/":
-        console.log("Loading home.js");
+      case "/index.html": // Add this line to handle index.html
         await import("./views/home/home.js");
         handleSkeletonLoader("skeleton-loader", "listings-container");
         break;
-      case "/login.html":
-        console.log("Loading login.js");
+      case "/login":
+      case "/login.html": // Add .html versions for all routes
         await import("./views/auth/login.js");
         handleSkeletonLoader("skeleton-loader", "login-form");
         break;
+      case "/register":
       case "/register.html":
-        console.log("Loading register.js");
         await import("./views/auth/register.js");
         handleSkeletonLoader("skeleton-loader", "register-form");
         break;
+      case "/profile":
       case "/profile.html":
-        console.log("Loading profile.js");
         await import("./views/profile/profile.js");
         handleSkeletonLoader("skeleton-loader", "profile-container");
         break;
+      case "/create-listing":
       case "/create-listing.html":
-        console.log("Loading createListing.js");
         await import("./views/listing/createListing.js");
         handleSkeletonLoader("skeleton-loader", "create-listing-form");
         break;
+      case "/single-listing":
       case "/single-listing.html":
-        console.log("Loading singleListing.js");
         await import("./views/listing/singleListing.js");
         handleSkeletonLoader("skeleton-loader", "listing-container");
         break;
+      case "/about":
       case "/about.html":
-        console.log("Loading singleListing.js");
-
         break;
       default:
-        console.error("Page not found:", pathname); // Debugging log
+        console.error("Page not found:", pathname);
         throw new Error("Page not found");
     }
   } catch (error) {
