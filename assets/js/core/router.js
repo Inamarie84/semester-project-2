5f05c7460691a42a3e9309a0e61760b347822be0
Inamarie84 <123692666+Inamarie84@users.noreply.github.com>
2025-02-25 18:43:16 +0100

    test

--- assets/js/core/router.js
+++ assets/js/core/router.js
@@ -1,49 +1,3 @@
-// import {
-//   showLoadingIndicator,
-//   hideLoadingIndicator,
-// } from "../utils/dom/loadingIndicator.js";
-// import { showMessage } from "../utils/dom/messageHandler.js"; // Import showMessage
-
-// export default async function router(pathname = window.location.pathname) {
-//   showLoadingIndicator(); // Show loading indicator before loading the module
-
-//   try {
-//     // Simulate network delay (e.g., 1 second)
-//     // await new Promise((resolve) => setTimeout(resolve, 1000));
-
-//     switch (pathname) {
-//       case "/":
-//         await import("./views/home/home.js");
-//         break;
-//       case "/auth/login.html":
-//         await import("./views/auth/login.js");
-//         break;
-//       case "/auth/register.html":
-//         await import("./views/auth/register.js");
-//         break;
-//       case "/profile/index.html":
-//         await import("./views/profile/profile.js");
-//         break;
-//       case "/listing/create-listing.html":
-//         await import("./views/listing/createListing.js");
-//         break;
-//       case "/listing/single-listing.html":
-//         await import("./views/listing/singleListing.js");
-//         break;
-//       case "/about/index.html":
-//         break;
-//       default:
-//         throw new Error("Page not found");
-//     }
-//   } catch (error) {
-//     console.error("Error loading page module:", error);
-//     showMessage("error", "error", "messages-container");
-//   } finally {
-//     hideLoadingIndicator();
-//   }
-// }
-
-// router.js
 import { showMessage } from "../utils/dom/messageHandler.js"; // Import showMessage
 import { handleSkeletonLoader } from "../utils/dom/skeletonLoader.js"; // Import handleSkeletonLoader
 
@@ -54,27 +8,25 @@ export default async function router(pathname = window.location.pathname) {
         await import("./views/home/home.js");
         handleSkeletonLoader("skeleton-loader", "listings-container"); // Handle skeleton loader for listings
         break;
-      case "/auth/login.html":
+      case "/auth/login":
         await import("./views/auth/login.js");
         handleSkeletonLoader("skeleton-loader", "login-form"); // Handle skeleton loader for login
         break;
-      case "/auth/register.html":
+      case "/auth/register":
         await import("./views/auth/register.js");
         handleSkeletonLoader("skeleton-loader", "register-form"); // Handle skeleton loader for registration
         break;
-      case "/profile/":
+      case "/profile":
         await import("./views/profile/profile.js");
         handleSkeletonLoader("skeleton-loader", "profile-container"); // Handle skeleton loader for profile
         break;
-      case "/listing/create-listing.html":
+      case "/listing/create-listing":
         await import("./views/listing/createListing.js");
         handleSkeletonLoader("skeleton-loader", "create-listing-form"); // Handle skeleton loader for create listing
         break;
-      case "/listing/single-listing.html":
+      case "/listing/single-listing":
         await import("./views/listing/singleListing.js");
         handleSkeletonLoader("skeleton-loader", "listing-container"); // Handle skeleton loader for single listing
-        break;
-      case "/about/index.html":
         break;
       default:
         throw new Error("Page not found");
